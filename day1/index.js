33705649a69d861e4d4bdd0f76e848ec894400ab
ShootingStar91 <ttu91@hotmail.com>
2023-12-07 23:32:16 +0200

    day 1 part 2 WIP

--- day1/index.js
+++ day1/index.js
@@ -1,23 +1,88 @@
 const fs = require("fs")
 
-const getRowNumber = row => {
-    const numbers = row.replace(/[^0-9]/g, '')
-    const first = numbers[0]
-    const last = numbers[numbers.length - 1]
+const numberWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+const digits = "123456789".split("")
+
+const getFirstDigit = (row, reverse, log) => {
+    let builder = ""
+    const reverseStr = str => reverse ? str.split("").reverse().join("") : str
+    let returnTo = -1
+    for (let i = reverse ? row.length - 1 : 0; reverse ? i >= 0 : i < row.length; reverse ? i-- : i++) {
+        let char = row[i]
+        
+        let temp = numberWords
+            .filter(nw => 
+                nw.length > builder.length && reverseStr(nw).startsWith(builder))
+
+        let possibleLetters = temp
+                    .map(str => reverseStr(str).charAt(builder.length))
+
+        if (log) console.log(`Current: ${char} \tBuilder: ${builder} \tPossible: ${possibleLetters.join(" ")} \t${temp} `)
+        
+        if (possibleLetters.includes(char)) {
+            builder += char
+        } else {
+            builder = char
+            let oldi = i
+            if (returnTo >= 0) i = returnTo
+            returnTo = oldi + (reverse ? -1 : 1)
+        }
+
+        if (numberWords.includes(reverseStr(builder))) return `${numberWords.indexOf(reverseStr(builder)) + 1}`
+        
+        if (digits.includes(char)) {
+            return char
+        }
+    }
+    return "empty"
+}
+
+const getRowNumber = row => {    
+    const first = getFirstDigit(row, false)
+    const last = getFirstDigit(row, true)
     return parseInt(`${first}${last}`, 10)
 }
 
-const compute = data => {
-    
+const compute = (data, log) => {    
     const rows = data.split("\n")
     let sum = 0
+    let i = 0
     for (const row of rows) {
-        sum += getRowNumber(row)
+        const rowNums = getRowNumber(row)
+        // console.log(row)
+        // console.log(rowNums)
+        if (log) console.log(row, rowNums)
+        console.log(i++, rowNums)
+        sum += rowNums
     }
     console.log(sum)
 }
 
 
-fs.readFile("../inputs/day1", 'utf8', (err, data) => {
-    compute(data)
-})
+const run = () => {
+    fs.readFile("../inputs/day1", 'utf8', (err, data) => {
+        compute(data, false)
+    })
+}
+const exampleData = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+
+const testOne = (str) => {
+    console.log(getFirstDigit(str, false, true))
+    console.log(getFirstDigit(str, true, true))
+    console.log(str)
+}
+
+const testExampleData = () => {
+    compute(exampleData)
+}
+
+testOne(`lhmgblplseventhreenine9vcpnpvzhjronenhczffqt`)
+
+
+
